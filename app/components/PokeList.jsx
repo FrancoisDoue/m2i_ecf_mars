dac0c881d171bdb735d71e327877d659dfb4c8ec
FrancoisDoue <doue.francois@outlook.com>
2024-03-27 07:47:13 +0100

    save 27-03 7:47

--- app/components/PokeList.jsx
+++ app/components/PokeList.jsx
@@ -1,51 +1,89 @@
-import { FlatList, RefreshControl } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import PokeItem from './PokeItem'
-import { useSelector } from 'react-redux'
+import {
+  Button,
+  FlatList,
+  Pressable,
+  RefreshControl,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import PokeItem from './PokeItem';
+import {useDispatch, useSelector} from 'react-redux';
+import {pokeColors} from '../styles/globalStyle';
+import {goToNextPage, goToPrevPage} from '../storage/slices/pokeSlice';
 
-const PokeList = ({list, headerComponent, footerComponent}) => {
+const PokeList = ({list}) => {
+  const dispatch = useDispatch();
 
-  const {step} = useSelector(state => state.pokemon)
+  const {step, page} = useSelector(state => state.pokemon);
 
-  // const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
-  //   // https://stackoverflow.com/questions/41056761/detect-scrollview-has-reached-the-end
-  //   const paddingToBottom = 100;
-  //   return layoutMeasurement.height + contentOffset.y >=
-  //     contentSize.height - paddingToBottom;
-  // };
-  // const [refreshing, setRefreshing] = useState(false)
-
-  // const onRefresh = React.useCallback(() => {
-  //   setRefreshing(true)
-  //   setTimeout(() => { setRefreshing(false) }, 2000)
-  // }, [])
-
-  // useEffect(() => {
-  //   console.log(refreshing)
-  // }, [refreshing])
+  const PressableFooter = () => {
+    return (
+      <Pressable>
+        <Text>Test</Text>
+      </Pressable>
+    );
+  };
 
   return (
     <FlatList
       data={list}
       numColumns={2}
-      ListHeaderComponent={ headerComponent || <></> }
-      ListFooterComponent={ (!!footerComponent && list?.length == step) ? footerComponent : <></> }
-      // onScroll={({nativeEvent}) => (!!onEnd && isCloseToBottom(nativeEvent)) && onEnd()} // pb de performance...
-      // onEndReachedThreshold={-.01}
-      // onStartReachedThreshold={.01}
-      // onStartReached={() => console.log('start')}
-      // onEndReached={() => console.log('hello')}
-      // scrollEventThrottle={200}
-      
-      // refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-      
-      columnWrapperStyle={{justifyContent: 'center'}}
-      keyExtractor={(item) => item.name}
-      renderItem={({item}) => 
-          <PokeItem pokemon={item} />
+      ListHeaderComponent={
+        page > 1 && (
+          <Button title="Précédents" color={pokeColors.pokeDarkRed} onPress={() => dispatch(goToPrevPage())} />
+        )
       }
+      ListFooterComponent={
+        list?.length == step && (
+          //   <Button title='Suivants' color={pokeColors.pokeBlue}  onPress={() => dispatch(goToNextPage())} />
+          <PressableFooter />
+        )
+      }
+      ListFooterComponentStyle={styles.footerComponent}
+      columnWrapperStyle={{justifyContent: 'center'}}
+      keyExtractor={item => item.name}
+      renderItem={({item}) => <PokeItem pokemon={item} />}
+      
     />
-  )
-}
+  );
+};
 
-export default PokeList
+export default PokeList;
+
+const styles = StyleSheet.create({
+  footerComponent: {
+    padding: 40,
+    width: '100%',
+  },
+});
+
+// const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
+//   // https://stackoverflow.com/questions/41056761/detect-scrollview-has-reached-the-end
+//   const paddingToBottom = 100;
+//   return layoutMeasurement.height + contentOffset.y >=
+//     contentSize.height - paddingToBottom;
+// };
+// const [refreshing, setRefreshing] = useState(false)
+
+// onScroll={({nativeEvent}) => (!!onEnd && isCloseToBottom(nativeEvent)) && onEnd()} // pb de performance...
+
+// -------------------------------------
+// const onRefresh = React.useCallback(() => {
+//   setRefreshing(true)
+//   setTimeout(() => { setRefreshing(false) }, 2000)
+// }, [])
+
+// useEffect(() => {
+//   console.log(refreshing)
+// }, [refreshing])
+
+// refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+
+// -----------------------------
+
+// onEndReachedThreshold={-.01}
+// onStartReachedThreshold={.01}
+// onStartReached={() => console.log('start')}
+// onEndReached={() => console.log('hello')}
+// scrollEventThrottle={200}
--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -1,30 +1,15 @@
-import { Button } from 'react-native'
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { goToNextPage, goToPrevPage } from '../storage/slices/pokeSlice'
+import { useSelector } from 'react-redux'
 import PokeList from '../components/PokeList'
 import GradientView from '../components/shared/GradientView'
-import { pokeColors } from '../styles/globalStyle'
 
 const HomeScreen = ({ navigation }) => {
 
-  const dispatch = useDispatch()
-  const {page, maxPage, pokeList} = useSelector(state => state.pokemon)
-
-  const handlePrev = () => dispatch(goToPrevPage())
-  const handleNext = () => dispatch(goToNextPage())
+  const {pokeList} = useSelector(state => state.pokemon)
 
   return (
     <GradientView>
       <PokeList 
-        headerComponent={
-          (page - 1) &&
-          <Button title='Précédents' color={pokeColors.pokeDarkRed}  onPress={handlePrev} />
-        }
-        footerComponent={
-          (page < maxPage) &&
-          <Button title='Suivants' color={pokeColors.pokeBlue} onPress={handleNext} />
-        }
         list={pokeList} 
         navigation={navigation}
       />
